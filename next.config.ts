870f4b536fac344188cc1581512253a66451ed9f
Miller <millermarru4@gmail.com>
2025-09-24 15:32:50 -0500

    Config: create to context

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  
+  // 🚀 Esto hace que Next genere HTML estático
+  output: "export",
+
+  // 👇 Esto evita que Next use el optimizador de imágenes en servidor
+  images: {
+    unoptimized: true,
+  },
+
+  // 👇 Hace que las rutas terminen en / (ej. /about/ en lugar de /about)
+  // y genera carpetas con index.html adentro → mejor compatibilidad en hosting compartidos
+  trailingSlash: true,
 };
 
 export default nextConfig;
